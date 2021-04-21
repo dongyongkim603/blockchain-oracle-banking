66d0114b31cbf9a5d79aae42232b8a54ed1cee60
John <dongyongkim8@gmail.com>
2021-04-21 02:10:36 -0400

    Employee_data view complete partial way through Employee_salary view

--- Blockchain_Finance.sql
+++ Blockchain_Finance.sql
@@ -1,7 +1,16 @@
-create view EMPLOYEE_DATA as;
+drop table branch;
+CREATE TABLE branch
+AS
+SELECT 	*
+FROM		PROJECT2020.branch;
+
+
+/*----------------------------------------------*/
+drop view EMPLOYEE_DATA;
+create view EMPLOYEE_DATA as
 
     select 
-    b.fname || ' ' || b.mname || ' ' || b.lname as "name of employee",
+    distinct nvl(b.fname, '') || ' ' || nvl(b.mname, '') || ' ' || nvl(b.lname, '') as "name of employee",
     b.street || ' ' || b.city || ', ' || b.city || ' ' || b.state as "address",
     b.zip as "Zip code of Employee Address",
     b.ssn as "SSN",
@@ -20,37 +29,70 @@ create view EMPLOYEE_DATA as;
     and c.empid = b.empid
     and rownum = 1) as "current year tax deduction",
     
-    b.hiredate as "Date employee was hired",
+    (select g.branchstartdate
+    from branch_employee g
+    where g.empid = b.empid) as "Date employee was hired",
+    
     b.dob as "birth date",
     trunc(((sysdate - b.dob)/365.25),0) as "age",
+    
     br.phoneext as "branch phone extension",
-    (select e.branchphone
+    
+    (select NVL(e.branchphone, 'no number availible')
     from branch e
-    where e.branchid = br.branchid) as "phone number"
+    where e.branchid = br.branchid) as "phone number",
+    
+    (select m.b_name
+    from branch m
+    where m.branchid = br.branchid) as "branch name",
+    
+    b.degree as "as highest degree earned",
+    
+    b.degreedate as "date earned degree"
     
     from Bank_employee b
-    inner join EMP_ANNUAL_DATA a
+    left join EMP_ANNUAL_DATA a
     on b.empid = a.empid
-    inner join branch_employee br
-    on br.empid = b.empid;
+    left join branch_employee br
+    on br.empid = b.empid
+    where b.ssn is not null;
     
-    
+    SELECT	 *
+    FROM 	EMPLOYEE_DATA;
+
         
 --*********************************************************
 
-    select * from BRANCH_EMPLOYEE;
-    select * from BRANCH;
+create view Employee_salary as;
     
-    select c.salary 
+    select 
+    distinct nvl(e.fname, '') || ' ' || nvl(e.mname, '') || ' ' || nvl(e.lname, '') as "name of employee",
+    
+    to_Char(sysdate, 'yyyy') as "current year",
+    
+    e.ssn as "employee ssn",
+    
+    (select c.salary 
     from EMP_ANNUAL_DATA c
     where c.year <= to_char(sysdate, 'YYYY')
-    and c.empid = b.empid
-    and rownum = 1
-    order by c.year desc
+    and c.empid = e.empid
+    and rownum = 1) as "current salary",
     
-    select c.salary 
-    from EMP_ANNUAL_DATA c
-    where c.year <= to_char(sysdate, 'YYYY')
-    and c.empid = 1000000001
-    and rownum = 1
-    order by c.year desc;
+    nvl((select m.b_name
+    from branch m
+    where m.branchid = b.branchid), 'no branch name availible') as "branch name",
+    
+    (select sum(nvl(g.salary,0))
+    from EMP_ANNUAL_DATA g
+    where g.empid = b.empid and b.branchid = e.empid) as "total salary at branch"
+    
+    from bank_employee e
+    left join branch_employee b
+    on e.empid = b.empid;
+    
+/*---------------------------------------------------*/
+    
+    select * from EMP_ANNUAL_DATA;
+    select * from bank_employee;
+    select * from branch;
+    select * from branch_employee;
