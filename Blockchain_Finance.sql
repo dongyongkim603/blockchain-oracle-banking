6395936a2d8a94ed0d6aeb3947bfc9a8ade387df
John <dongyongkim8@gmail.com>
2021-04-24 14:42:37 -0400

    created employee_salary and branch_data views

--- Blockchain_Finance.sql
+++ Blockchain_Finance.sql
@@ -63,7 +63,7 @@ create view EMPLOYEE_DATA as
         
 --*********************************************************
 
-create view Employee_salary as;
+create view Employee_salary as
     
     select 
     distinct nvl(e.fname, '') || ' ' || nvl(e.mname, '') || ' ' || nvl(e.lname, '') as "name of employee",
@@ -82,17 +82,74 @@ create view Employee_salary as;
     from branch m
     where m.branchid = b.branchid), 'no branch name availible') as "branch name",
     
-    (select sum(nvl(g.salary,0))
-    from EMP_ANNUAL_DATA g
-    where g.empid = b.empid and b.branchid = e.empid) as "total salary at branch"
+   nvl((select sum(ema.salary)
+    from EMP_ANNUAL_DATA ema
+    left join branch_employee br
+    on ema.empid = br.empid
+    where br.branchid  = (select x.branchid
+                          from branch_employee x 
+                          where e.empid = x.empid)),0)
+    as "total branch salaries",
+   
+    nvl((select max(ema.salary)
+    from EMP_ANNUAL_DATA ema
+    left join branch_employee br
+    on ema.empid = br.empid
+    where br.branchid  = (select x.branchid
+                          from branch_employee x 
+                          where e.empid = x.empid)),0)
+    as "highest branch salary",
+    
+    trunc(nvl((select avg(ema.salary)
+    from EMP_ANNUAL_DATA ema
+    left join branch_employee br
+    on ema.empid = br.empid
+    where br.branchid  = (select x.branchid
+                          from branch_employee x 
+                          where e.empid = x.empid)),0), 2)
+    as "average branch salary"
     
     from bank_employee e
     left join branch_employee b
-    on e.empid = b.empid;
+    on e.empid = b.empid
+    left join branch a
+    on b.branchid = a.branchid;
     
+    select * from Employee_salary;
 /*---------------------------------------------------*/
     
+create view Branch_data as;
+    select 
+    br.branchid,
+    br.b_name as "branch name",
+    br.b_st || ' ' || br.b_city || ', ' || br.b_state || ', ' || br.b_zip as "branch address",
+    nvl(br.branchphone, 'no phone listed') as "branch phone",
+    (select count(*)
+    from branch_employee be
+    where br.branchid = be.branchid) as "number of branch employees",
+    br.category,
+    be.fname || ' ' || be.lname as "brnach manager",
+    
+    (select count(*)
+    from DEPOSIT_ACCT_TRANSACTION dat
+    where dat.accessptid = bap.accesspointid) as "number of branch transactions"
+    
+    from branch br
+    left join  branch_manager bm
+    on bm.branchid =br.branchid
+    left join bank_employee be
+    on bm.empid = be.empid
+    left join branch_access_points bap
+    on br.branchid = bap.branchid;
+    
+/*---------------------------------------------------*/
     select * from EMP_ANNUAL_DATA;
     select * from bank_employee;
     select * from branch;
     select * from branch_employee;
+    select * from DEPOSIT_ACCT_TRANSACTION;
+    select * from CREDIT_ACCT_TRAnSACTION;
+    select * from DEPOSIT_ACCt; 
+    select * from DEPOSIT_ACCt_PRODUCT;
+    select * from credit_account;
+    select * from credit_product;
