5d59ed79187a3a2b63238c1a2b59963ef2d54148
John <dongyongkim8@gmail.com>
2021-04-26 21:56:25 -0400

    created the two custom customer views

--- Blockchain_Finance.sql
+++ Blockchain_Finance.sql
@@ -6,6 +6,7 @@ FROM		PROJECT2020.branch;
 
 
 /*----------------------------------------------*/
+
 drop view EMPLOYEE_DATA;
 create view EMPLOYEE_DATA as
 
@@ -117,13 +118,16 @@ create view Employee_salary as
     
     select * from Employee_salary;
 /*---------------------------------------------------*/
-    
-create view Branch_data as;
+alter table branch
+add fax_number number;
+create or replace view Branch_data as
     select 
     br.branchid,
     br.b_name as "branch name",
     br.b_st || ' ' || br.b_city || ', ' || br.b_state || ', ' || br.b_zip as "branch address",
     nvl(br.branchphone, 'no phone listed') as "branch phone",
+    br.fax_number as "branch fax number",
+    (select fax_number from dual) as "fax number",
     (select count(*)
     from branch_employee be
     where br.branchid = be.branchid) as "number of branch employees",
@@ -142,14 +146,109 @@ create view Branch_data as;
     left join branch_access_points bap
     on br.branchid = bap.branchid;
     
+    select *
+    from Branch_data;
+    
 /*---------------------------------------------------*/
+
+alter table bank_customer
+add email varchar2(512);
+
+create or replace view Valued_Customers as
+    select distinct
+    bc.ssn,
+    bc.fname || '  ' || bc.lname as "name",
+    trunc(months_between(sysdate, bc.dob)/12, 0) as "age",
+    bc.homephone as "home phone",
+    bc.workphone as "work phone",
+    bc.street || ' ' || bc.city as "address",
+    bc.zip,
+    bc.email,
+    bc.state,
+    
+    (select count(cat.amount)
+    from credit_acct_transaction cat
+    where to_char(cast(cat.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1
+    and cat.creditacctno = ca.creditacctno)
+    +
+    (select count(dt.amount)
+    from DEPOSIT_ACCT_TRANSACTION dt
+    where to_char(cast(dt.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1
+    and ltrim(dt.acctno, 'CDEP0') = ltrim(ca.creditacctno, 'CCR0')) as "Total transactions",
+    
+    nvl((select sum(dt.amount) 
+    from deposit_acct_transaction dt
+    where to_char(cast(dt.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1
+    and ltrim(dt.acctno, 'CDEP0') = ltrim(ca.creditacctno, 'CCR0')),0)
+    +
+    nvl((select sum(cat.amount)
+    from credit_acct_transaction cat
+    where cat.creditacctno = ca.creditacctno
+    and to_char(cast(cat.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1),0)
+    as "Total transaction amount"
+    
+    from bank_customer bc
+    left join credit_account ca
+    on bc.custid = ca.primary
+    left join DEPOSIT_ACCt da
+    on bc.custid =da.primary
+    where nvl((select sum(dt.amount) 
+    from deposit_acct_transaction dt
+    where to_char(cast(dt.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1
+    and ltrim(dt.acctno, 'CDEP0') = ltrim(ca.creditacctno, 'CCR0')),0)
+    +
+    nvl((select sum(cat.amount)
+    from credit_acct_transaction cat
+    where cat.creditacctno = ca.creditacctno
+    and to_char(cast(cat.transdatetime as date), 'YYYY') = to_char(sysdate, 'YYYY')-1),0) > 500;
+    
+    select * from valued_customers;
+
+/*---------------------------------------------------*/
+
+create or replace view Statistics_by_Branch as
+    select 
+    b.branchid,
+    b.b_name,
+    to_char(sysdate, 'yyyy') as  "year",
+    (select count(*) 
+    from DEPOSIT_ACCT_TRANSACTION da
+    left join branch_access_points bap
+    on da.accessptid = bap.accesspointid
+    where bap.branchid = b.branchid) as "deposits at branch",
+    
+    ((select count(*)
+    from credit_acct_transaction cat)
+    +
+    (select count(*)
+    from DEPOSIT_ACCT_TRANSACTION dt
+    )) as "total transactions",
+    
+    (select count(*) 
+    from bank_employee be
+    left join branch_employee br
+    on be.empid =br.empid
+    where br.branchid = b.branchid) as "total employees at branch"
+    from branch b;
+    
+    select * from Statistics_by_Branch;
+    
+/*---------------------------------------------------*/
+
     select * from EMP_ANNUAL_DATA;
     select * from bank_employee;
-    select * from branch;
     select * from branch_employee;
-    select * from DEPOSIT_ACCT_TRANSACTION;
     select * from CREDIT_ACCT_TRAnSACTION;
     select * from DEPOSIT_ACCt; 
+    select * from DEPOSIT_ACCT_TRANSACTION;
+    select * from credit_account;
     select * from DEPOSIT_ACCt_PRODUCT;
     select * from credit_account;
+    select * from branch;
     select * from credit_product;
+    select * from bank_customer;
+    select * from loan_payment;
+    select * from bank_customer;
+    select * from atm;
+    select * from branch_access_points;
+    select * from DEPOSIT_ACCT_TRANSACTION;
